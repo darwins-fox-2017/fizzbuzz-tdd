2ce0b047c54d18404c5c454c02e3d95e0a606fb4
mfpebrizal <ftm.pebrizal@gmail.com>
2017-03-13 14:13:21 +0700

    refactor minor

--- test/fizzBuzz-test.js
+++ test/fizzBuzz-test.js
@@ -3,28 +3,28 @@ const should = chai.should()
 const fizzBuzz = require('../fizzBuzz');
 
 describe('FizzBuzz', function () {
-  it('should return Fizz',function () {
+  it('should return "Fizz"',function () {
     fizzBuzz.convert(3).should.equal('Fizz')
     fizzBuzz.convert(9).should.equal('Fizz')
     fizzBuzz.convert(12).should.equal('Fizz')
     fizzBuzz.convert(21).should.equal('Fizz')
   })
 
-  it('should return Buzz',function () {
+  it('should return "Buzz"',function () {
     fizzBuzz.convert(25).should.equal('Buzz')
     fizzBuzz.convert(5).should.equal('Buzz')
     fizzBuzz.convert(10).should.equal('Buzz')
     fizzBuzz.convert(125).should.equal('Buzz')
   })
 
-  it('should return Buzz',function () {
+  it('should return "FizzBuzz"',function () {
     fizzBuzz.convert(15).should.equal('FizzBuzz')
     fizzBuzz.convert(60).should.equal('FizzBuzz')
     fizzBuzz.convert(105).should.equal('FizzBuzz')
     fizzBuzz.convert(90).should.equal('FizzBuzz')
   })
 
-  it('should return Buzz',function () {
+  it('should return nothing',function () {
     fizzBuzz.convert(4).should.equal('')
     fizzBuzz.convert(28).should.equal('')
     fizzBuzz.convert(2).should.equal('')
