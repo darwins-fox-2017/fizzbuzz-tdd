1cc298b2b3ba02f8833f43173352d78b2fd42347
isumizumi <isumi.karina72@gmail.com>
2017-03-13 11:52:32 +0700

    TDD - Part 3: Testing function Buzz

--- test/testing_fizzbuzz.js
+++ test/testing_fizzbuzz.js
@@ -5,4 +5,8 @@ describe('Testing function fizz', () => {
   it('should return Fizz if input can multiple by 3', () => {
     converter.convert(6).should.equal("Fizz")
   })
+
+  it('should return Buzz if input can multiple by 5', () => {
+    converter.convert(10).should.equal("Buzz")
+  })
 })
