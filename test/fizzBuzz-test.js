10da6e644556c9b06fc7a86e6f49e2e72799dc75
mfpebrizal <ftm.pebrizal@gmail.com>
2017-03-13 12:34:56 +0700

    Done

---
+++ test/fizzBuzz-test.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const should = chai.should()
+const fizzBuzz = require('../fizzBuzz');
+
+describe('FizzBuzz', function () {
+  it('should return Fizz',function () {
+    fizzBuzz.convert(3).should.equal('Fizz')
+    fizzBuzz.convert(9).should.equal('Fizz')
+    fizzBuzz.convert(12).should.equal('Fizz')
+    fizzBuzz.convert(21).should.equal('Fizz')
+  })
+
+  it('should return Buzz',function () {
+    fizzBuzz.convert(25).should.equal('Buzz')
+    fizzBuzz.convert(5).should.equal('Buzz')
+    fizzBuzz.convert(10).should.equal('Buzz')
+    fizzBuzz.convert(125).should.equal('Buzz')
+  })
+
+  it('should return Buzz',function () {
+    fizzBuzz.convert(15).should.equal('FizzBuzz')
+    fizzBuzz.convert(60).should.equal('FizzBuzz')
+    fizzBuzz.convert(105).should.equal('FizzBuzz')
+    fizzBuzz.convert(90).should.equal('FizzBuzz')
+  })
+
+  it('should return Buzz',function () {
+    fizzBuzz.convert(4).should.equal('')
+    fizzBuzz.convert(28).should.equal('')
+    fizzBuzz.convert(2).should.equal('')
+    fizzBuzz.convert(16).should.equal('')
+  })
+})
